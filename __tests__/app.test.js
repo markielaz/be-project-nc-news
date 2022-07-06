d4965668ce10166f99dd5c21640bb4f3c135bc64
Mark <mark_lazarus@hotmail.com>
2022-07-06 16:08:59 +0100

    removed console logs, added article id assertion to test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -184,6 +184,7 @@ describe('GET /api/articles/:article_id/comments', () => {
             created_at: expect.any(String),
             author: expect.any(String),
             body: expect.any(String),
+            article_id: 1
           })
         )
       })
--- app.js
+++ app.js
@@ -40,7 +40,6 @@ app.use((err, req, res, next) => {
   });
   
   app.use((err, req, res, next) => {
-    console.log(err);
     res.status(500).send({ msg: 'Internal Server Error' });
   });
 
